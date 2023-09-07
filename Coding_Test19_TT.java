411891d63d01528face08429712ce1e36916a02d
madola3813 <madola3813@gmail.com>
2023-09-07 16:20:12 +0900

    230907 이재빈 오전 테스트 코딩

---
+++ Coding_Test19_TT.java
@@ -0,0 +1,40 @@
+//Q19. for문과 continue문을 사용하여
+//2부터 9까지의 구구단 중에서 짝수 단만 출력하는
+//자바 프로그램을 제작해 봅니다.
+
+public class Coding_Test19 {
+
+	public static void main(String[] args) {
+//		for (int dan = 2; dan < 10; dan++) {
+//			if(dan%2 != 0) {
+//				continue;
+//			}
+//			for (int i = 1; i < 10; i++) {
+//				int result = dan*i;
+//				System.out.println(dan + " x " + i + " = " + result);
+//			}
+//		}
+		
+//---------------------------------------------		풀이 
+		int dan; // dan(단) 값을 담을 변수를 선언합니다.
+		int times; // times(곱할값) 값을 담을 변수를 선언합니다.
+		
+		for (dan = 2; dan <= 9; dan++) {
+			// 만약에 dan값을 2로 나눈 나머지가 0이 아니라면(홀수라면)
+			if (dan%2 != 0) {
+				// 위에 if문에서 홀수라면 continue문으로 건너띄고(제외하고),
+				// 계속해서 for문을 반복 수행 처리하게 합니다.
+				continue;
+			}
+//			System.out.println(dan);
+			for (times = 1; times <= 9; times++) {
+				System.out.println(dan + " X " + times + " = " + dan*times);
+			}
+			System.out.println();
+		}
+		
+
+
+
+	}
+}

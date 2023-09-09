03cc839b7046366b27f8ff0e847c05e169756fd9
jby585748 <jjboyeong@gmail.com>
2023-09-09 13:44:17 +0900

    20230908 1팀 스터티 test1, test2 정답 공유

--- TEAM1_STUDY/src/test02_jby.java
+++ TEAM1_STUDY/src/test02_jby.java
@@ -1,21 +1,18 @@
-
 //다음은 컴퓨터 게임의 병사(marine)를 클래스로 정의한 것이다.
 //이 클래스의 멤버중에 static을 붙여야 하는 것은 어떤 것들이고 그 이유는 무엇인가?
 //(단, 모든 병사의 공격력과 방어력은 같아야한다.)
 
-public class test02_jby {
-
-class Marine{
-	int x = 0, y = 0;
+class Marine;
+	int x = 0, y = 0; // 컴퓨터게임의 병사들은 모두 위치가 다르기때문에 다른 x, y값을 가짐
 	int hp = 60;
-	int weapon = 6;
-	int armor = 0;
+	static int weapon = 6; // 공격력과 방어력은  모든 병사가 같아야 하므로 static 붙여야 함
+	static int armor = 0;// 공격력과 방어력은  모든 병사가 같아야 하므로 static 붙여야 함
 	
-	void weaponUp() {
+	static void weaponUp() { // static 필드를 이용하는 메서드이므로   static 입력
 		weapon++;
 	}
 	
-	void armorUp() {
+	static void armorUp() { // static 필드를 이용하는 메서드이므로   static 입력
 		armor++;
 	}
 	
@@ -23,7 +20,4 @@ class Marine{
 		this.x = x;
 		this.y = y;
 	}
-	
-}
-
 }
